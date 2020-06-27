1ec653890e7679d0c881e13062c0a7756d86cc54
higgers10 <41135378+higgers10@users.noreply.github.com>
2020-06-27 09:25:48 -0400

    Separated Terrain Generator

---
+++ versions/Terrain Generation/source/Craft.Net.Common/ParticleEffect.cs
@@ -0,0 +1,55 @@
+﻿namespace Craft.Net.Common
+{
+    public class ParticleEffect
+    {
+        public const int Explode = 0;
+        public const int LargeExplode = 1;
+        public const int HugeExplosion = 2;
+        public const int FireworksSpark = 3;
+        public const int Bubble = 4;
+        public const int Splash = 5;
+        public const int Wake = 6;
+        public const int Suspended = 7;
+        public const int DeathSuspend = 8;
+        public const int Crit = 9;
+        public const int MagicCrit = 10;
+        public const int Smoke = 11;
+        public const int LargeSmoke = 12;
+        public const int Spell = 13;
+        public const int InstantSpell = 14;
+        public const int MobSpell = 15;
+        public const int MobSpellAmbient = 16;
+        public const int WitchMagic = 17;
+        public const int DripWater = 18;
+        public const int DripLava = 19;
+        public const int AngryVillager = 20;
+        public const int HappyVillager = 21;
+        public const int TownAura = 22;
+        public const int Note = 23;
+        public const int Portal = 24;
+        public const int EnchantmentTable = 25;
+        public const int Flame = 26;
+        public const int Lava = 27;
+        public const int Footstep = 28;
+        public const int Cloud = 29;
+        public const int RedDust = 30;
+        public const int SnowballPoof = 31;
+        public const int SnowShovel = 32;
+        public const int Slime = 33;
+        public const int Heart = 34;
+        public const int Barrier = 35;
+        
+        //iconcrack_(id)_(data)?
+        public const int IronCrack = 36;
+        //blockcrack_(id+(data<<12))?
+        public const int BlockCrack = 37;
+        //blockdust_(id)?
+        public const int BlockDust = 38;
+
+        public const int Droplet = 39;
+        public const int Take = 40;
+        public const int MobApperance = 41;
+
+
+    }
+}
---
+++ versions/Terrain Generation/source/Craft.Net.Common/Position.cs
@@ -0,0 +1,24 @@
+﻿namespace Craft.Net.Common
+{
+    public class Position
+    {
+        public long Encoded;
+        public Vector3 VecPosition;
+
+        public Position(long x, long y, long z)
+        {
+            Encoded = ((x & 0x3FFFFFF) << 38) | ((z & 0x3FFFFFF) << 12) | (y & 0xFFF);
+            VecPosition = new Vector3(x, y, z);
+        }
+
+        public Position(long encoded)
+        {
+            Encoded = encoded;
+            long val = Encoded;
+            long x = val >> 38;
+            long y = val & 0xFFF;
+            long z = (val << 26 >> 38);
+            VecPosition = new Vector3(x, y, z);
+        }
+    }
+}
